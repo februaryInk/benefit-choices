901655be9f8e22468067c31542ec73081268b56b
februaryInk <febrink@gmail.com>
2016-11-01 19:57:51 -0400

    Improve the FAQ section.

--- app/assets/javascripts/ready.js
+++ app/assets/javascripts/ready.js
@@ -9,3 +9,21 @@ $(document).on('page:load', function () {
     new ratingMeter(element, 5);
   });
 });
+
+$( document ).on( 'mouseenter', '.js-expandable', function ( evt, el ) {
+    $( this ).addClass( '-hover' );
+} );
+
+$( document ).on( 'mouseleave', '.js-expandable', function ( evt, el ) {
+    $( this ).removeClass( '-hover' );
+} );
+
+$( document ).on( 'click', '.js-expandable', function ( evt, el ) {
+    if ( $( this ).hasClass( '-open' ) ) {
+        $( this ).find( '.js-expandable-hidden' ).slideUp( 250 );
+        $( this ).removeClass( '-open' );
+    } else {
+        $( this ).find( '.js-expandable-hidden' ).slideDown( 250 );
+        $( this ).addClass( '-open' );
+    }
+} );
