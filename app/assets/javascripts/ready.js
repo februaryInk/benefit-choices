3beb7b1328171a1b4f4588a7b507f3aa318bd2be
februaryInk <febrink@gmail.com>
2016-11-17 14:51:21 -0500

    Make header more responsive.

--- app/assets/javascripts/ready.js
+++ app/assets/javascripts/ready.js
@@ -19,6 +19,19 @@ $( document ).on( 'mouseleave', '.js-expandable', function ( evt, el ) {
     $( this ).removeClass( '-hover' );
 } );
 
+$( document ).on( 'click', '.js-nav-toggle', function ( evt, el ) {
+    
+    var target = $( this ).data( 'target' );
+    
+    if ( $( this ).hasClass( '-open' ) ) {
+        $( this ).removeClass( '-open' );
+        $( target ).slideUp( 500 );
+    } else {
+        $( this ).addClass( '-open' );
+        $( target ).slideDown( '-open' );
+    }
+} );
+
 // detect the open link and style it a certain way.
 function openLink( path ) {
     
